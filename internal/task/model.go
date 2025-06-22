93dc170ce20704db24a1c972506501348aac5bb8
vyudin <vyudin@syntellect.ru>
2025-06-22 15:28:17 +0400

    rename status

--- internal/task/model.go
+++ internal/task/model.go
@@ -2,20 +2,20 @@ package task
 
 import "time"
 
-type TaskStatus string
+type Status string
 
 const (
-	StatusPending   TaskStatus = "pending"
-	StatusRunning   TaskStatus = "running"
-	StatusCompleted TaskStatus = "completed"
-	StatusDeleted   TaskStatus = "deleted"
+	StatusPending   Status = "pending"
+	StatusRunning   Status = "running"
+	StatusCompleted Status = "completed"
+	StatusDeleted   Status = "deleted"
 )
 
 type Task struct {
-	ID             string     `json:"id"`
-	Status         TaskStatus `json:"status"`
-	CreatedAt      time.Time  `json:"created_at"`
-	Duration       string     `json:"duration"`
-	Result         string     `json:"result,omitempty"`
-	ProcessingTime int        `json:"processing_time"`
+	ID             string    `json:"id"`
+	Status         Status    `json:"status"`
+	CreatedAt      time.Time `json:"created_at"`
+	Duration       string    `json:"duration"`
+	Result         string    `json:"result,omitempty"`
+	ProcessingTime int       `json:"processing_time"`
 }
